cdc5e29ddb1812479b24f38cc2c625fffe5c6f4d
samkelo <samkelo.nhlapo@gmail.com>
2023-08-16 15:08:05 -0700

    alphabet function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,14 @@
-#include 
+#include "main.h"
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for(letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putcher('\n');
+}
